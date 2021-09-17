f65ca67dccdbec346c705e3839a5ecfd48bb30f9
vcoppe <vianney.coppe@gmail.com>
2021-09-17 16:27:28 +0200

    Clean AccessibilityChecker and compute value lazily

--- src/main/java/graph/AccessibilityChecker.java
+++ src/main/java/graph/AccessibilityChecker.java
@@ -12,29 +12,20 @@ public class AccessibilityChecker {
 
     private final Stock stock;
     private final Graph graph;
-    private int id;
-    private HashMap<Vector3D, Integer> component;
-    private HashMap<Integer, Boolean> accessible;
+    private final HashMap<Vector3D, Vector3D> component;
+    private final HashMap<Vector3D, Boolean> accessible;
 
     public AccessibilityChecker(Stock stock, Graph graph) {
-        this.id = 0;
         this.stock = stock;
         this.graph = graph;
         this.component = new HashMap<>();
         this.accessible = new HashMap<>();
     }
 
-    private int getId() {
-        int currentId = this.id;
-        this.id++;
-        if (this.id < 0) this.id = 0;
-        return currentId;
-    }
-
     private void resetSet(Vector3D position) {
         if (this.component.containsKey(position)) {
-            int component = this.component.get(position);
-            this.accessible.remove(component);
+            this.accessible.remove(this.component.get(position));
+            this.component.remove(position);
         }
     }
 
@@ -53,6 +44,14 @@ public class AccessibilityChecker {
     }
 
     public boolean check(Vector3D position) {
+        if (this.component.containsKey(position)) {
+            if (this.accessible.containsKey(this.component.get(position))) {
+                return this.accessible.get(this.component.get(position));
+            } else {
+                this.component.remove(position);
+            }
+        }
+
         Pallet pallet = this.stock.get(position);
         if (pallet == null) {
             return true;
@@ -60,10 +59,10 @@ public class AccessibilityChecker {
             for (Edge edge : this.graph.getReverseEdges(position)) {
                 Vector3D v = edge.to;
                 pallet = this.stock.get(v);
-                if (pallet == null) {
+                if (pallet == null) { // found exit
                     return true;
                 } else if (this.stock.isFree(v)) {
-                    if (check(v)) {
+                    if (check(v)) { // check neighbour
                         return true;
                     }
                 }
@@ -71,26 +70,16 @@ public class AccessibilityChecker {
             return false;
         }
 
-        /*if (this.component.containsKey(position)) {
-            int component = this.component.get(position);
-            Boolean access = this.accessible.get(component);
-            if (access != null) {
-                return access;
-            }
-        }*/
-
-        //boolean accessible = false;
-        //int component = this.getId();
-        int accesses = 0;
-        int locks = 0;
-
+        // forbid access to subgraphs where a location is already locked
+        // TODO do not forbid but set a precedence constraint
+        int accesses = 0, locks = 0;
 
         Stack<Vector3D> stack = new Stack<>();
         HashSet<Vector3D> visited = new HashSet<>();
 
         stack.push(position);
         visited.add(position);
-        //this.component.put(position, component);
+        this.component.put(position, position);
 
         while (!stack.empty()) {
             Vector3D u = stack.pop();
@@ -98,20 +87,14 @@ public class AccessibilityChecker {
             for (Edge edge : this.graph.getReverseEdges(u)) {
                 Vector3D v = edge.to;
 
-                //Integer currentComponent = this.component.get(v);
-
-                if (!visited.contains(v)/*currentComponent == null || currentComponent != component*/) {
-                    /*if (currentComponent != null) {
-                        this.resetSet(v);
-                    }*/
+                if (!visited.contains(v)) {
                     visited.add(v);
+                    this.component.put(v, position);
                     pallet = this.stock.get(v);
-                    if (pallet == null) { // found exit
-                        //accessible = true;
+                    if (pallet == null) {
                         accesses++;
-                    } else if (this.stock.isFree(v)) { // TODO do something if locked?
+                    } else if (this.stock.isFree(v)) {
                         stack.push(v);
-                        //this.component.put(v, component);
                     } else if (this.stock.isLocked(v)) {
                         locks++;
                     }
@@ -119,10 +102,21 @@ public class AccessibilityChecker {
             }
         }
 
-        //this.accessible.put(component, accessible);
+        /*boolean nextToPallet = false;
+        for (Edge edge : this.graph.getReverseEdges(position)) {
+            Vector3D v = edge.to;
+            pallet = this.stock.get(v);
+            if (pallet != null && pallet != Pallet.FREE) {
+                nextToPallet = true;
+                break;
+            }
+        }*/
 
-        //return accessible;
-        return locks < accesses;
+        boolean accessible = locks < accesses;// && (accesses > 1 || nextToPallet);
+
+        this.accessible.put(position, accessible);
+
+        return accessible;
     }
 
 }
